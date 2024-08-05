5eb4e6db39b8196390d95f8e5396232a84ad0194
yonemoto <x-na-yonemoto@kddi-tech.com>
2024-08-05 11:46:35 +0900

    part #1

--- app/src/main/java/com/example/workshopsample1/MainActivity.kt
+++ app/src/main/java/com/example/workshopsample1/MainActivity.kt
@@ -11,5 +11,32 @@ class MainActivity : AppCompatActivity() {
 	//ライフサイクルのイベントは必ずオーバーライドになる
 	override fun onCreate(savedInstanceState : Bundle?) {
 		super.onCreate(savedInstanceState)
+		
+		//　ボタンの動作を設定する
+		val colorChangeButton = findViewById<Button>(R.id.color_change_button)
+		val textView = findViewById<TextView>(R.id.main_title_text)
+		colorChangeButton.setOnClickListener {
+			textView.setTextColor(getColor(R.color.red))
+		}
+		
+		val textChangeButton = findViewById<Button>(R.id.text_change_button)
+		onChangeTextWord(textChangeButton, textView, "WORLD, HELLO!")
 	}
+	
+	//メソッド化
+	//Androidは大抵onCreate,onResumeに処理がものすごい偏るので長くなりそうならメソッドに切り出していく
+	private fun onChangeTextWord(button : Button, textView : TextView, text: String){
+		button.setOnClickListener {
+			textView.text = text
+		}
+	}
+	
+	override fun onResume() {
+		super.onResume()
+	}
+	
+	override fun onDestroy() {
+		super.onDestroy()
+	}
+	
 }
